fd16beaa58f10714f9dd8bc31297891058be48f0
岩田一宏 <kazuiwa1031@gmail.com>
2017-07-30 10:54:54 +0900

    test

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,30 @@
+#coding: utf-8
 class ClientsController < ApplicationController
   def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
 
   end
 end
