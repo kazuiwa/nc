06927ac34fc40a6710bb47d23a9406ddd7963bb4
岩田一宏 <kazuiwa1031@gmail.com>
2017-08-13 19:40:55 +0900

    動機プルダウン変更時のアクション

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,3 +1,19 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function(){
+    if ($("#client-new").length) {
+        //動機プルダウン変更時のアクション
+        $("#client_douki").change(function () {
+            //紹介の場合
+            if ($("#client_douki").val() == 4) {
+                $("#form-shoukaisha").css('display', 'block');
+                $("#form-douki-etc").css('display', 'none')
+            //その他の場合
+            } else if ($("#client_douki").val() == 6) {
+                $("#form-shoukaisha").css('display', 'none');
+                $("#form-douki-etc").css('display', 'block');
+            } else {
+                $("#form-shoukaisha").css('display', 'none');
+                $("#form-douki-etc").css('display', 'none');
+            }
+        });
+    }
+});
